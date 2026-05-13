bccf1031a852674b55b896acd256a340b7af7ca1
Kshitij <a678t@unb.ca>
2026-05-13 15:59:25 -0300

    Question1 code structure

---
+++ lab2/question1/question1.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(void){
+
+    printf("Value of A is %d \n", 'A');
+    printf("Value of B is %d \n", 'B');
+    printf("Value of C is %d \n", 'C');
+    printf("Value of a is %d \n", 'a');
+    printf("Value of b is %d \n", 'b');
+    printf("Value of c is %d \n", 'c');
+    printf("Value of 0 is %d \n", '0');
+    printf("Value of 1 is %d \n", '1');
+    printf("Value of 2 is %d \n", '2');
+    printf("Value of $ is %d \n", '$');
+    printf("Value of * is %d \n", '*');
+    printf("Value of + is %d \n", '+');
+    printf("Value of / is %d \n", '/');
+    printf("Value of space is %d \n", ' ');
+
+    return 0;
+}
+
