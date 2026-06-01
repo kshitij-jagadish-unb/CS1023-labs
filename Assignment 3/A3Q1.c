ca99f66d512654bf3ac4bca962d1caf62a72703a
Kshitij <a678t@unb.ca>
2026-05-31 22:18:34 -0300

    A3Q1 Complete code structure

--- Assignment 3/A3Q1.c
+++ Assignment 3/A3Q1.c
@@ -11,3 +11,19 @@ int gcd(int x, int y) {
     }
 }
 
+int main(void) {
+    
+    int x = 0;
+    int y = 0;
+
+    printf("Gimme your first number: ");
+    scanf("%d", &x);
+
+    printf("Gimme your second number: ");
+    scanf("%d", &y);
+
+    printf("Greatest Common Divisor of the numbers is: ");
+    printf("%d\n", gcd(x, y));
+
+    return 0;
+}
