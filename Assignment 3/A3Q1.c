ada9b06ac78dadd24c91b848ef5f54c3e91e8254
Kshitij <a678t@unb.ca>
2026-05-31 22:24:34 -0300

    A3Q1 Slight code change and comments

--- Assignment 3/A3Q1.c
+++ Assignment 3/A3Q1.c
@@ -1,28 +1,29 @@
 #include <stdio.h>
 
-int gcd(int x, int y) {
+int gcd(int x, int y) {   //Greatest Common Diviser Function
     if (y == 0) {
 
-        return x;
+        printf("Undefined Buddy.");
+        return x;    //Anything divided by 0 is undefined
     }
     else {
 
-        return gcd(y, x % y);
+        return gcd(y, x % y);  //Divide
     }
 }
 
-int main(void) {
+int main(void) {     //Greatest Common Diviser Main Function
     
     int x = 0;
     int y = 0;
 
-    printf("Gimme your first number: ");
-    scanf("%d", &x);
+    printf("Gimme your first number: ");     //Asking for the 1st number
+    scanf("%d", &x);                      
 
-    printf("Gimme your second number: ");
+    printf("Gimme your second number: ");      //Asking for the 2nd number
     scanf("%d", &y);
 
-    printf("Greatest Common Divisor of the numbers is: ");
+    printf("Greatest Common Divisor of the numbers is: ");     //Returning the divided number
     printf("%d\n", gcd(x, y));
 
     return 0;
