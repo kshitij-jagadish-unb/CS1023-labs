f131d3304915bc927b2850a657b08e9a3aa50e88
Kshitij <a678t@unb.ca>
2026-05-31 22:50:49 -0300

    A3Q2 Complete code structure

--- Assignment 3/A3Q2.c
+++ Assignment 3/A3Q2.c
@@ -12,3 +12,20 @@ int power(int base, int exponent) {
         return base * power(base, exponent - 1);
     }
 }
+
+int main(void) {     
+    
+    int base = 0;
+    int exponent = 0;
+
+    printf("Gimme your base number: ");     
+    scanf("%d", &base);                      
+
+    printf("Gimme your exponental increment: ");      
+    scanf("%d", &exponent);
+
+    printf("Your Answer is: ");     
+    printf("%d\n", power(base, exponent));
+
+    return 0;
+}
