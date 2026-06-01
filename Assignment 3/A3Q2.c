69e609f47d690a7aa834651ff0413344200d3e8a
Kshitij <a678t@unb.ca>
2026-05-31 22:53:41 -0300

    A3Q2 Code comments

--- Assignment 3/A3Q2.c
+++ Assignment 3/A3Q2.c
@@ -1,21 +1,21 @@
 #include <stdio.h>
 
-int power(int base, int exponent) {
+int power(int base, int exponent) {   //power function
 
     // Base case
     if (exponent == 1) {
 
-        return base;
+        return base;         //anything raised to 1 is the same thing
     }
     else {
 
-        return base * power(base, exponent - 1);
+        return base * power(base, exponent - 1);   //returns the exponential 
     }
 }
 
 int main(void) {     
     
-    int base = 0;
+    int base = 0;              //asking for the base and exponent
     int exponent = 0;
 
     printf("Gimme your base number: ");     
@@ -24,7 +24,7 @@ int main(void) {
     printf("Gimme your exponental increment: ");      
     scanf("%d", &exponent);
 
-    printf("Your Answer is: ");     
+    printf("Your Answer is: ");               //returns it 
     printf("%d\n", power(base, exponent));
 
     return 0;
