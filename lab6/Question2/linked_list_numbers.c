e9ca85907d14edcf33bfbc0cee3f94f01d504f10
Kshitij <a678t@unb.ca>
2026-06-20 17:21:05 -0300

    L6Q2 appendnode func

--- lab6/Question2/linked_list_numbers.c
+++ lab6/Question2/linked_list_numbers.c
@@ -7,7 +7,7 @@ struct node {
 };
 
 struct node *createNode(int value) {
-    
+
     struct node *new_node = (struct node *)malloc(sizeof(struct node));
     
     if (new_node != NULL) {
@@ -19,3 +19,27 @@ struct node *createNode(int value) {
     return new_node;
 }
 
+void appendNode(struct node **head, int value) {
+    struct node *new_node = createNode(value);
+    
+    if (new_node == NULL) {
+        printf("Error: Memory allocation failed for value %d\n", value);
+        return;
+
+    if (*head == NULL) {
+      
+        *head = new_node;
+    } 
+    else {
+        
+        struct node *current_node = *head;
+        
+       
+        while (current_node->next != NULL) {
+            current_node = current_node->next;
+        }
+        
+        
+        current_node->next = new_node;
+    }
+}
