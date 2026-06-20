e1a63880e2a620903279ade8ddb48a820518727d
Kshitij <a678t@unb.ca>
2026-06-20 17:33:09 -0300

    L6Q2 Main function

--- lab6/Question2/linked_list_numbers.c
+++ lab6/Question2/linked_list_numbers.c
@@ -105,3 +105,64 @@ void freeList(struct node *head) {
     }
 }
 
+int main(void) {
+
+    struct node *head = NULL; 
+    
+    int num_items = 0;
+    int input_check = 0;
+
+    printf("How many integers do you want to add to the list? ");
+    input_check = scanf("%d", &num_items);
+
+    if (input_check != 1) {
+
+        printf("Error: You did not enter a valid number.\n");
+        return 1;
+    } 
+    if (num_items <= 0) {
+
+        printf("Notice: Creating an empty list.\n");
+    }
+    else {
+
+        int index = 0;
+        int user_value = 0;
+
+        while (index < num_items) {
+
+            printf("Enter integer %d: ", index + 1);
+            scanf("%d", &user_value);
+            
+            appendNode(&head, user_value);
+            
+            index = index + 1;
+        }
+    }
+
+    printf("\n--- Linked List Data ---\n");
+    printList(head);
+
+    printf("Total number of nodes: %zu\n", countNodes(head));
+    printf("Sum of all values: %d\n", sumNodes(head));
+
+    printf("\n--- Search ---\n");
+    int search_target = 0;
+    printf("Enter a value to search for: ");
+    scanf("%d", &search_target);
+
+    int found = searchNode(head, search_target);
+
+    if (found == 1) {
+
+        printf("Result: The value %d WAS found in the list.\n", search_target);
+    } 
+    else {
+        
+        printf("Result: The value %d was NOT found in the list.\n", search_target);
+    }
+
+    freeList(head);
+    
+    return 0;
+}
